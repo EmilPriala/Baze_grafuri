3ec67e21ae9b50f4c0dfe75a61ff37561a4e3c8a
Emil <Emil@MYDESKTOP>
2023-10-12 19:44:22 +0300

    Add project files.

---
+++ Edge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baze_grafuri
+{
+    public  class Edge
+    {
+        public Vertex begin, end;
+
+        public Edge(Vertex begin, Vertex end) 
+        {
+            this.begin = begin;
+            this.end = end;
+        }
+    }
+}
---
+++ Vertex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baze_grafuri
+{
+    public class Vertex
+    {
+        public int value;
+        public int X, Y;
+        public Vertex(int v) 
+        {
+            value = v;
+        }
+        public Vertex(int value, int x, int y) : this(value)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
